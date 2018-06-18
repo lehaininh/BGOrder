7c197d890ada5881d2198efddbf094c57bd71f7f
Le Hai Ninh <ninh.contact@gmail.com>
2018-06-18 09:59:35 +0700

    Created API for retrieving order by ID

--- controller/orders.ctrl.js
+++ controller/orders.ctrl.js
@@ -1,11 +1,45 @@
 const logger = require("../util/logger.js");
-const addressesService = require("../service/addresses.service.js");
+const ordersService = require("../service/orders.service.js");
 
 const addressesController = {
 	handleGetOrderByOrderID: (req, res) => {
+		const startTime = Date.now();
+		const { order_id } = req.params;
+		ordersService.getOrderByOrderIDs(order_id)
+			.then(response => {
+				const return_data = {
+					total: response.length,
+					message: "OK",
+					orders: response
+				};
+				const endTime = Date.now();
+				logger.info("handleGetOrderByOrderID succeeded in: ", endTime - startTime, "ms");
+				res.status(200).send(return_data);
+			})
+			.catch(err => {
+				logger.error("Error handleGetOrderByOrderID: ", err);
+				res.status(500).send("Error");
+			});
 	},
 
 	handleCreateOrder: (req, res) => {
+		//const startTime = Date.now();
+		//const { customer_id } = req.params;
+		//customersService.getOrdersByCustomerID(customer_id)
+			//.then(response => {
+				//const return_data = {
+					//total: response.length,
+					//message: "OK",
+					//orders: response
+				//};
+				//const endTime = Date.now();
+				//logger.info("handleGetCustomersOrders succeeded in: ", endTime - startTime, "ms");
+				//res.status(200).send(return_data);
+			//})
+			//.catch(err => {
+				//logger.error("Error handleGetCustomersOrders: ", err);
+				//res.status(500).send("Error");
+			//});
 	},
 };
 
--- service/orders.service.js
+++ service/orders.service.js
@@ -2,13 +2,13 @@ const ordersModel = require("../model/orders.model.js");
 const populateSerivce = require("./populate.service.js");
 const logger = require("../util/logger.js");
 
-const customersService = {
-	getOrdersFromAnAddress: address_id => {
-		return ordersModel.getOrdersByAddressID(address_id)
+const ordersService = {
+	getOrderByOrderIDs: order_id => {
+		return ordersModel.getOrderByOrderIDs([order_id])
 			.then(orders => {
 				return populateSerivce.populateInfoForOrders(orders);
 			});
 	},
 };
 
-module.exports = customersService;
+module.exports = ordersService;
